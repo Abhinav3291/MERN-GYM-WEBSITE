3d259267752f17514aa5b6cfb2d17a55c9c91ca5
Abhinav3291 <122594780+Abhinav3291@users.noreply.github.com>
2025-02-04 12:17:21 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -8,13 +8,16 @@ const router = express.Router();
 
 config({ path: "./config/config.env" });
 
-app.use(
-  cors({
-    origin: ["https://tech-gym-fro.vercel.app"],
-    methods: ["POST"],
-    credentials: true,
-  })
-);
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://tech-gym-fro.vercel.app'); // or '*'
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(204);
+    }
+    next();
+});
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
